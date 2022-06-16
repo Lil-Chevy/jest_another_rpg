97b59d8f0cc1be55036ff23dc55220ff1dddbf35
cheddar <nowaczewskijack@gmail.com>
2022-06-15 22:09:39 -0400

    add potion constructor and function

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,16 +1,25 @@
-const Potion = require('../lib/Potion.js');
+const Player = require("../lib/Player.js");
+const Potion = require("../lib/Potion.js");
 
-test('creates a health potion object', () => {
-  const potion = new Potion('health');
+test("creates a health potion object", () => {
+  const potion = new Potion("health");
 
-  expect(potion.name).toBe('health');
+  expect(potion.name).toBe("health");
   expect(potion.value).toEqual(expect.any(Number));
 });
 
-test('creates a random potion object', () => { 
+test("creates a random potion object", () => {
   const potion = new Potion();
 
   expect(potion.name).toEqual(expect.any(String));
   expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
+});
+test("adds a potion to the inventory", () => {
+  const player = new Player("Dave");
+  const oldCount = player.inventory.length;
+
+  player.addPotion(new Potion());
+
+  expect(player.inventory.length).toBeGreaterThan(oldCount);
 });
